827cfbf95fc26d412a088d7b9c25fae80d9ea16f
SrikanthGowda01 <Srigowda.2735@gmail.com>
2021-08-19 09:27:41 +0530

    sec commit

---
+++ Js/Controlflow/asc3.js
@@ -0,0 +1,8 @@
+let num1=1, num2=2, num3=3;
+if(num1<num2 && num1<num3){
+    console.log(num1,num2,num3);
+}else if(num2>num1 && num2<num3){
+    console.log(num2,num1,num3);
+}else{
+    console.log(num3,num2,num1);
+}
