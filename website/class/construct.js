209ae2158ddc5446dc6bc3f35b9c00755f05ab64
SrikanthGowda01 <srigowda.2735@gmail.com>
2021-09-01 20:47:27 +0530

    comt

---
+++ website/class/construct.js
@@ -0,0 +1,11 @@
+class Test {
+    constructor() {
+      console.log("I am good boy");
+    }
+    wish() {
+      console.log("Good Morning");
+    }
+  }
+  
+  let obj1 = new Test();
+console.log(obj1);
